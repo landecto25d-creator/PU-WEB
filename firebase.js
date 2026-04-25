31917acb9aa889a6b7e0212e7ae378664cb9b031
Tigor <landecto25d.student.unhas.ac.id>
2026-04-25 16:51:00 +0800

    Fix iOS mobile click issue and change Firebase to CDN imports

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, doc, setDoc, increment } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
+import { getFirestore, doc, setDoc, increment } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
